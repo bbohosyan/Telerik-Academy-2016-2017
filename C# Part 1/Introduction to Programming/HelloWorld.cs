0db414f29f5310ca6fc55172be3417bf1fe06e73
siqqQ <gprex1@yahoo.com>
2016-04-20 14:33:42 +0300

    HelloWorld.cs

--- C# Part 1/Introduction to Programming/HelloWorld.cs
+++ C# Part 1/Introduction to Programming/HelloWorld.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace TelerikPractice
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, C#!");
         }
     }
 }

c19b9be88ffc73c532ff3ec6943a04470b0f4bb5
siqqQ <gprex1@yahoo.com>
2016-05-26 20:20:58 +0300

    C# Part 2 Homework 2

---
+++ C# Part 2/02. Multidimensional Arrays/Fill the matrix.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char wayOfBuilding = char.Parse(Console.ReadLine());
+        int[,] block = new int[n, n];
+        int printNum = 1;
+        if (wayOfBuilding == 'a')
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    block[j, i] = printNum;
+                    printNum++;
+                }
+            }
+        }
+        else if (wayOfBuilding == 'b')
+        {
+            bool isUpToDown = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (isUpToDown == true)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        block[j, i] = printNum;
+                        printNum++;
+                    }
+                    isUpToDown = false;
+                }
+                else
+                {
+                    for (int j = n - 1; j >= 0; j--)
+                    {
+                        block[j, i] = printNum;
+                        printNum++;
+                    }
+                    isUpToDown = true;
+                }
+
+            }
+        }
+        else if (wayOfBuilding == 'c')
+        {
+            for (int i = n - 1; i >= 0; i--)//n - 1 = 1,n - 2 = 2
+            {
+                int row = i;
+                int col = 0;
+                for (int j = 0; j < n - i; j++)
+                {
+                    block[row, col] = printNum;
+                    printNum++;
+                    row++;
+                    col++;
+                }
+            }
+            for (int i = 0; i < n - 1; i++)
+            {
+                int row = 0;
+                int col = i + 1;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    block[row, col] = printNum;
+                    printNum++;
+                    row++;
+                    col++;
+                }
+            }
+        }
+        else if (wayOfBuilding == 'd')
+        {
+            char direction = 'd';
+            int col = 0;
+            int row = 0;
+            for (int i = 0; i < n * n; i++)
+            {
+                if (direction == 'd')
+                {
+                    block[row, col] = i + 1;
+                    row++;
+                    if ((row == n) || (block[row, col] != 0))
+                    {
+                        direction = 'r';
+                        row--;
+                        col++;
+                    }
+                }
+                else if (direction == 'r')
+                {
+                    block[row, col] = i + 1;
+                    col++;
+                    if ((col == n) || (block[row, col] != 0))
+                    {
+                        direction = 'u';
+                        col--;
+                        row--;
+                    }
+
+                }
+                else if (direction == 'u')
+                {
+                    block[row, col] = i + 1;
+                    row--;
+                    if ((row == -1) || (block[row, col] != 0))
+                    {
+                        direction = 'l';
+                        row++;
+                        col--;
+                    }
+
+                }
+                else if (direction == 'l')
+                {
+                    block[row, col] = i + 1;
+                    col--;
+                    if (block[row, col] != 0)
+                    {
+                        direction = 'd';
+                        col++;
+                        row++;
+                    }
+
+                }
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                Console.Write(block[i, j] + " ");
+            }
+            Console.Write(block[i, n - 1]);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Part 2/02. Multidimensional Arrays/Maximal sum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nAndM = Console.ReadLine();
+        string[] numberNandM = nAndM.Split(' ');
+        int rows = int.Parse(numberNandM[0]);
+        int cols = int.Parse(numberNandM[1]);
+        int[,] array = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            string currentLine = Console.ReadLine();
+            string[] currentLineNumbers = currentLine.Split(' ');
+            for (int j = 0; j < cols; j++)
+            {
+                array[i, j] = int.Parse(currentLineNumbers[j]);
+            }
+        }
+        int row = 0;
+        int col = 0;
+        int maxSum = int.MinValue;
+        while (true)
+        {
+            int currentSum = 0;
+            currentSum = array[row, col] + array[row + 1, col] + array[row + 2, col] + array[row, col + 1] + array[row + 1, col + 1]
+            + array[row + 2, col + 1] + array[row, col + 2] + array[row + 1, col + 2] + array[row + 2, col + 2];
+            if (currentSum > maxSum)
+            {
+                maxSum = currentSum;
+            }
+            col++;
+            if (col > cols - 3)
+            {
+                row++;
+                col = 0;
+            }
+
+            if ((row == rows - 3) && (col == cols - 3))
+            {
+                break;
+            }
+        }
+        Console.WriteLine(maxSum);
+    }
+}

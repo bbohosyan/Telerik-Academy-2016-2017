ea265f98de2be2724c92db2deadb614ca97b8756
siqqQ <gprex1@yahoo.com>
2016-06-01 16:58:44 +0300

    C#2 07. Homework

---
+++ C# Part 2/07. Exception Handling/Enter numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+class ExtractSentences
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        bool isException = false;
+        if (numbers[0] > 0 && numbers[numbers.Length - 1] < 100 && numbers[0] < numbers[1])
+        {
+            for (int i = 1; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] >= numbers[i + 1])
+                {
+                    isException = true;
+                }
+            }
+            if (isException)
+            {
+                Console.WriteLine("Exception");
+            }
+            else
+            {
+                Console.Write(1 + " < ");
+                for (int z = 0; z < numbers.Length; z++)
+                {
+                    Console.Write(numbers[z] + " < ");
+                }
+                Console.WriteLine("100");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Exception");
+        }
+
+    }
+}
---
+++ C# Part 2/07. Exception Handling/Square root.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+class ExtractSentences
+{
+    static void Main()
+    {
+        string a = Console.ReadLine();
+        double b;
+        if (double.TryParse(a, out b) == false)
+        {
+            Console.WriteLine("Invalid number");
+            Console.WriteLine("Good bye");
+        }
+        else
+        {
+            b = double.Parse(a);
+            if (b < 0)
+            {
+                Console.WriteLine("Invalid number");
+                Console.WriteLine("Good bye");
+            }
+            else
+            {
+                Console.WriteLine("{0:F3}", Math.Sqrt(b));
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}

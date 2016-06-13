329470edf2f5446cd9710a1794ee77a86d4cb3f7
siqqQ <gprex1@yahoo.com>
2016-06-13 17:01:59 +0300

    C# OOP Homework No.1

---
+++ C# OOP/Defining Classes - Part 1/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileDevice
+{
+    public class GlobalConstants
+    {
+        public const string NoInformation = "no info";
+        public const string NullValue = "cannot be null or empty string";
+        public const string NegativeNumber = "should be a positive numbber";
+    }
+}
---
+++ C# OOP/Defining Classes - Part 1/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace MobileDevice
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var tests = new GsmTests();
+            tests.DisplayGsmInformation();
+
+            var call = new Call(new DateTime(2015, 09, 15, 12, 54, 6), "0456-345-33", 390);
+            Console.WriteLine(call.ToString());
+
+            var callTest = new GsmCallHistoryTest();
+            callTest.Functionality();
+        }
+    }
+}
